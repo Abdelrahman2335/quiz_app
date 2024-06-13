f9e3a009bf0600ed22249eb3236fa88451067b91
Abdelrahman2335 <125411627+Abdelrahman2335@users.noreply.github.com>
2024-06-13 04:14:05 +0300

    Update QuestionPage.dart

--- lib/models/QuestionPage.dart
+++ lib/models/QuestionPage.dart
@@ -6,7 +6,7 @@ class QuestionPage{
   final List<String> answers;
 
   List <String> get shuffledAnswers{
-    final shuffledList= List.of(answers); // to make a cope of a list 
+    final shuffledList= List.of(answers); // to make a copy of a list 
     shuffledList.shuffle(); // shuffled is void so we have to make a var in this case it's "shuffledList" and then use shuffle() than return the var
     return shuffledList;
   }
